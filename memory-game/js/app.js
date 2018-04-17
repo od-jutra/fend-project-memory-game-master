bd9df87360a23fe69584de470f7ce5365b3e91db
od-jutra <odjutra.odpisuje@gmail.com>
2018-04-17 21:19:25 +0200

    after 1st appraisal
    solved issue with star grade and  style

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -21,15 +21,14 @@ function shuffle(array) {
   
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
   
     return array;
@@ -61,7 +60,6 @@ for (var i = 0; i < 16; i++){
 };
 
 //flip card   idea how to flip from https://jsfiddle.net/james2doyle/qsQun/
-
 var visible = false;
 let visibleCards = [];
 for (var i = 0; i < 16; i++){
@@ -87,28 +85,31 @@ function countMoves(a){
     movesNum.innerHTML = move + ' moves';
     countStars()
 };
+
+// stars
 const starOne = document.querySelector('.firstStar');
 
 function countStars(){
-    if(move >= 20 && move <= 50){
-        starOne.style.display = 'none'
+    if(move >= 30 && move <= 50){
+        starOne.style.color = '#999'
     }
     else if(move > 50 ){
         const starTwo = document.querySelector('.secondStar');
-        starTwo.style.display = 'none';
-        starOne.style.display = 'none'
+        starTwo.style.display = ' #999';
+        starOne.style.display = ' #999'
     }
  };
+
 // to check if two cards match
 let matchedCards = [];
 function checkCards(){
     if(visibleCards.length == 2){
         if(visibleCards[0].classList[1] == visibleCards[1].classList[1]){
-           for(i=0; i<2; i++){
-            visibleCards[i].classList.add('match');
-            matchedCards.push(visibleCards[i])}
-            visibleCards.length = 0;   /* method 2 form https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript */
-            ifMatched();
+            for(i=0; i<2; i++){
+                visibleCards[i].classList.add('match');
+                matchedCards.push(visibleCards[i])}
+                visibleCards.length = 0;   /* method 2 form https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript */
+                ifMatched();
         }
         else{
             hideCard(visibleCards[0]);
@@ -119,42 +120,41 @@ function checkCards(){
 };
 
 // hide a card 
-
 function hideCard(i){
     setTimeout(function(){i.classList.remove('open')}, 1000)
 };
-// check the number of matched cards
 
+// check the number of matched cards
 function ifMatched(){
         console.log('cards matched!');
         whenFinish()
 };
-    
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) 
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 
 //  reload
 let reloadButton = document.querySelector('.restart');
 reloadButton.addEventListener('click', function(){
     window.location.reload(true);  
 });
-
 // mdn- reload  https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
 
+// congrats box
+
+// function to display congrats box
 const modal = document.querySelector('.modal');
 const playAgain = document.querySelector('.playAgain');
 const close = document.getElementsByClassName("close")[0];
-
-// function to display congrats box
+let starNum;
+function savedStars(){
+    if(move >= 30 && move <= 50){
+        starNum = 2
+    }
+    else if(move > 50 ){
+       starNum = 1
+    }
+    else{
+        starNum = 3
+    };
+};
 
 function whenFinish(){
     if(matchedCards.length == 16){
@@ -162,21 +162,22 @@ function whenFinish(){
             modal.style.display = "block";
             let scoresMessage = document.querySelector('.scores');
             let realSec = sec - 2;
-            scoresMessage.innerHTML = 'You did it in '+ realSec + ' seconds and ' + move + ' moves';
+            savedStars();
+            scoresMessage.innerHTML = 'Congrats! You did it in '+ realSec + ' seconds and ' + move + ' moves. You saved ' + starNum + '&#9733;';
         }, 1500)
     }
 };
 
-playAgain.onclick = function(){
+playAgain.addEventListener('click', function(){
     window.location.reload(true); 
-};
+});
 
-close.onclick = function(){
+close.addEventListener('click', function(){
     modal.style.display = "none";
-};
+});
 
-window.onclick = function(event){
+window.addEventListener('click', function(event){
     if (event.target == modal) {
         modal.style.display = "none";
     }
-};
+});
